510b08081d8d763b6bb8983b43a16c62277a3440
Alex Kott <tetlokax@gmail.com>
2018-08-24 15:24:55 +0530

    add missing babel presets

--- src/config/webpack.config.dev.js
+++ src/config/webpack.config.dev.js
@@ -58,6 +58,7 @@ module.exports = {
             loader: require.resolve('babel-loader'),
             options: {
               cacheDirectory: true,
+              presets: ['env', 'react'],
             },
           },
           {
